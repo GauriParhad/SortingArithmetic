c3bcbacef44cb119ebeeab75d9bd82e5ad5b05e2
Gaurip10 <thegauriparhad@gmail.com>
2021-07-26 19:13:55 +0530

    adding array.sh

---
+++ array.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -x
+
+read -p "Enter value of a: " a 
+read -p "Enter value of b: " b
+read -p "Enter value of c: " c
+
+A=$(($a + $b * $c))
+B=$(($a * $b + $c))
+C=$(($c + $a / $b))
+D=$(($a % $b + $c))
+
+declare -A resulted_Sum 
+resulted_Sum[1]=$A
+resulted_Sum[2]=$B
+resulted_Sum[3]=$C
+resulted_Sum[4]=$D
+
+declare -a arr_Sum
+for ((i=1; i<5; i++))
+do
+	arr_Sum[$i]=${resulted_Sum[$i]}
+done
