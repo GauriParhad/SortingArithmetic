df9281b1c946744b83ffe48ae1c7265b47067bc6
Gaurip10 <thegauriparhad@gmail.com>
2021-07-26 18:51:41 +0530

    adding compute1.sh

---
+++ compute1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -x
+
+echo "Welcome to Sorting Arithmetic Computation Problem"
+
+read -p "Enter value of a: " a 
+read -p "Enter value of b: " b
+read -p "Enter value of c: " c
+
+A=$(($a + $b * $c))
